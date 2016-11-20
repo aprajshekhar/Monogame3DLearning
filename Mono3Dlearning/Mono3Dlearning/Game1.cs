e001875e100015225a3bc43eb26c5f068ea90163
gaurav dubey <gaurav.dubey99@gmail.com>
2016-11-21 01:07:02 +0530

    Aligned Models properly and added sample ground.

--- Mono3Dlearning/Mono3Dlearning/Game1.cs
+++ Mono3Dlearning/Mono3Dlearning/Game1.cs
@@ -4,15 +4,27 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Mono3Dlearning
 {  
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class Game1 : Game
     {
+
+        //FOr controlling the width/Height and other graphics related topic.
         GraphicsDeviceManager graphics;
 
+        // For rendering any sprites.
         SpriteBatch spriteBatch;
+
+        //Created an instance of Model to load 3D model in game.
         Model alienModel;
+
+        //Creating a vertex Array for creating floor.
+        VertexPositionTexture[] vPTexture;
+
+        //Creating BasicEffect instance which will hold parameters for rendering such as position and lighting
+        BasicEffect effects;
+
+        //The Tiles Texture
+        Texture2D checkerboardTexture;
+        Vector3 cameraPosition = new Vector3(0, 40, 20);
 
         public Game1()
         {
@@ -20,62 +32,98 @@ namespace Mono3Dlearning
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
+        {            
+            vPTexture = new VertexPositionTexture[6];
+            vPTexture[0].Position = new Vector3(-20, -20, 0);
+            vPTexture[1].Position = new Vector3(-20, 20, 0);
+            vPTexture[2].Position = new Vector3(20, -20, 0);
+            vPTexture[3].Position = vPTexture[1].Position;
+            vPTexture[4].Position = new Vector3(20, 20, 0);
+            vPTexture[5].Position = vPTexture[2].Position;
+          
+            int repetitions = 10;
 
+            vPTexture[0].TextureCoordinate = new Vector2(0, 0);
+            vPTexture[1].TextureCoordinate = new Vector2(0, repetitions);
+            vPTexture[2].TextureCoordinate = new Vector2(repetitions, 0);
+
+            vPTexture[3].TextureCoordinate = vPTexture[1].TextureCoordinate;
+            vPTexture[4].TextureCoordinate = new Vector2(repetitions, repetitions);
+            vPTexture[5].TextureCoordinate = vPTexture[2].TextureCoordinate;
+          
+            effects = new BasicEffect(graphics.GraphicsDevice);
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             alienModel = Content.Load<Model>("robot");
-            // TODO: use this.Content to load your game content here
+            using (var stream = TitleContainer.OpenStream("Content/checkerboard.png"))
+            {
+                checkerboardTexture = Texture2D.FromStream(this.GraphicsDevice, stream);
+            }
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            //Clearing Color in the game after each update of draw to remove unwanted color left in the screen.
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            DrawGround();
+            DrawAlienModel(new Vector3(0, 0,5));
+            DrawAlienModel(new Vector3(0, -10, 5));
+            DrawAlienModel(new Vector3(0, 10, 5));
+            DrawAlienModel(new Vector3(-10, 0, 5));
+            DrawAlienModel(new Vector3(10, 0, 5));
+
+            base.Draw(gameTime);
+        }
+        //Drawing Ground
+        void DrawGround()
+        {
+            var cameraLookAt = Vector3.Zero;
+            var cameraUp = Vector3.UnitZ;
+
+            effects.View = Matrix.CreateLookAt(cameraPosition, cameraLookAt, cameraUp);
+
+            float aspectRatio = graphics.PreferredBackBufferWidth / (float)graphics.PreferredBackBufferHeight;
+            float fieldOfView = MathHelper.PiOver4;
+            float nearClipPlane = 1;
+            float farClipPlane = 200;
+            effects.Projection =  Matrix.CreatePerspectiveFieldOfView(fieldOfView, aspectRatio, nearClipPlane, farClipPlane);
+
+            effects.TextureEnabled = true;
+            effects.Texture = checkerboardTexture;
+
+            foreach(var pass in effects.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                graphics.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList,
+            // The array of verts that we want to render
+            vPTexture,
+            // The offset, which is 0 since we want to start 
+            // at the beginning of the floorVerts array
+            0,
+            // The number of triangles to draw
+            2);
+            }
+        }
+
+        //Drawing alien model
+        void DrawAlienModel(Vector3 position)
+        {
             foreach (var mesh in alienModel.Meshes)
             {
                 foreach (BasicEffect bEffect in mesh.Effects)
@@ -83,9 +131,8 @@ namespace Mono3Dlearning
 
                     bEffect.EnableDefaultLighting();
                     bEffect.PreferPerPixelLighting = true;
-                    bEffect.World = Matrix.Identity;
+                    bEffect.World = Matrix.CreateTranslation(position);
 
-                    var cameraPosition = new Vector3(0, 8, 0);
                     var cameraUpPosition = Vector3.UnitZ;
                     var cameraLookAt = Vector3.Zero;
 
@@ -100,9 +147,6 @@ namespace Mono3Dlearning
                 }
                 mesh.Draw();
             }
-            // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
         }
     }
 }
